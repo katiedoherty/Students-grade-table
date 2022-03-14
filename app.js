efaddd76110ae1ee2528c15f04dc16169729463a
katiedoherty <katie.doherty@mycit.ie>
2022-03-14 20:55:08 +0000

    added some styling

--- app.js
+++ app.js
@@ -81,16 +81,32 @@
     var grades9 = parseInt(0);
     var gradeslength9=parseInt(0);
     var addedgrades9=0;
+
+
+    var unsubmittedcount=45;
     function entergrades(clickedid){
-      var grade1 = prompt("Enter your grade for assignment");
+      var grade1 = parseInt(prompt("Enter your grade for assignment"));
+      if(isNaN(grade1)){
+        grade1="N/A";
+      }
+
+      if(grade1=="N/A"){
+        document.getElementById(clickedid).innerText = "-";
+      }
+      else{
+
       if(grade1<60){
         document.getElementById(clickedid).className = "below60";
-        console.log(document.getElementById(clickedid).className);
+        
+      }
+      else{
+        document.getElementById(clickedid).className = "above60";
       }
       if(document.getElementById(clickedid).innerText!="-"){
         changedassignment = parseInt(document.getElementById(clickedid).innerText);
         changedlength =parseInt(1);
       }
+      
       else{
         changedassignment=parseInt(0);
         changedlength=parseInt(0);
@@ -104,60 +120,85 @@
         gradeslength=gradeslength+parseInt(1)-changedlength;
         addedgrades=parseInt(addedgrades)+parseInt(grade1)-parseInt(changedassignment);
         grades = addedgrades/gradeslength;
+        
+        
         document.getElementById('finalgrade').innerText = Math.round(grades)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield6"||clickedid=="inputfield7"||clickedid=="inputfield8"||clickedid=="inputfield9"||clickedid=="inputfield10"){
         gradeslength2=gradeslength2+parseInt(1)-changedlength;
         addedgrades2=parseInt(addedgrades2)+parseInt(grade1)-parseInt(changedassignment);
         grades2 = addedgrades2/gradeslength2;
         document.getElementById('finalgrade2').innerText = Math.round(grades2)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield11"||clickedid=="inputfield12"||clickedid=="inputfield13"||clickedid=="inputfield14"||clickedid=="inputfield15"){
         gradeslength3=gradeslength3+parseInt(1)-changedlength;
         addedgrades3=parseInt(addedgrades3)+parseInt(grade1)-parseInt(changedassignment);
         grades3 = addedgrades3/gradeslength3;
         document.getElementById('finalgrade3').innerText = Math.round(grades3)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield16"||clickedid=="inputfield17"||clickedid=="inputfield18"||clickedid=="inputfield19"||clickedid=="inputfield20"){
         gradeslength4=gradeslength4+parseInt(1)-changedlength;
         addedgrades4=parseInt(addedgrades4)+parseInt(grade1)-parseInt(changedassignment);
         grades4 = addedgrades4/gradeslength4;
         document.getElementById('finalgrade4').innerText =Math.round(grades4)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield21"||clickedid=="inputfield22"||clickedid=="inputfield23"||clickedid=="inputfield24"||clickedid=="inputfield25"){
         gradeslength5=gradeslength5+parseInt(1)-changedlength;
         addedgrades5=parseInt(addedgrades5)+parseInt(grade1)-parseInt(changedassignment);
         grades5 = addedgrades5/gradeslength5;
         document.getElementById('finalgrade5').innerText = Math.round(grades5)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield26"||clickedid=="inputfield27"||clickedid=="inputfield28"||clickedid=="inputfield29"||clickedid=="inputfield30"){
         gradeslength6=gradeslength2+parseInt(1)-changedlength;
         addedgrades6=parseInt(addedgrades6)+parseInt(grade1)-parseInt(changedassignment);
         grades6 = addedgrades6/gradeslength6;
         document.getElementById('finalgrade6').innerText = Math.round(grades6)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments"+unsubmittedcount
       }else if(clickedid=="inputfield31"||clickedid=="inputfield32"||clickedid=="inputfield33"||clickedid=="inputfield34"||clickedid=="inputfield35"){
         gradeslength7=gradeslength7+parseInt(1)-changedlength;
         addedgrades7=parseInt(addedgrades7)+parseInt(grade1)-parseInt(changedassignment);
         grades7 = addedgrades7/gradeslength7;
         document.getElementById('finalgrade7').innerText = Math.round(grades7)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
       }
       else if(clickedid=="inputfield36"||clickedid=="inputfield37"||clickedid=="inputfield38"||clickedid=="inputfield39"||clickedid=="inputfield40"){
         gradeslength8=gradeslength8+parseInt(1)-changedlength;
         addedgrades8=parseInt(addedgrades8)+parseInt(grade1)-parseInt(changedassignment);
         grades8 = addedgrades8/gradeslength8;
         document.getElementById('finalgrade8').innerText = Math.round(grades8)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments"+unsubmittedcount
       }
       else if(clickedid=="inputfield41"||clickedid=="inputfield42"||clickedid=="inputfield43"||clickedid=="inputfield44"||clickedid=="inputfield45"){
         gradeslength9=gradeslength9+parseInt(1)-changedlength;
         addedgrades9=parseInt(addedgrades9)+parseInt(grade1)-parseInt(changedassignment);
         grades9 = addedgrades9/gradeslength9;
         document.getElementById('finalgrade9').innerText = Math.round(grades9)+"%";
+        unsubmittedcount=unsubmittedcount-1;
+        document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount
         
       }
   
-     
+    }
       
     }
+
+    //the unsubmitted count
+    if(unsubmittedcount==45){
+    document.getElementById("unsubmittedcount").innerText = "Number of unsubmitted Assignments: "+unsubmittedcount;}
+    
 
     
     
