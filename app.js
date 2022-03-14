c8ced4dfda130be3c834df7143f1500c867bc7f4
katiedoherty <katie.doherty@mycit.ie>
2022-03-14 14:06:31 +0000

    added in final grade calculator

--- app.js
+++ app.js
@@ -9,18 +9,32 @@
         return result;
     }
     document.getElementById("studentid1").innerHTML=studentid();
-    //var grades = 0;
-   
-    
+    var grades = parseInt(0);
+    var gradeslength=parseInt(0);
+    var addedgrades=0;
+    var changedassignment=0;
+    var changedlength=0;
     function entergrades(clickedid){
       var grade1 = prompt("Enter your grade for assignment");
+      if(document.getElementById(clickedid).innerText!="-"){
+        changedassignment = parseInt(document.getElementById(clickedid).innerText);
+        changedlength =parseInt(1);
+      }
+      else{
+        changedassignment=parseInt(0);
+        changedlength=parseInt(0);
+      }
       document.getElementById(clickedid).innerText = grade1+"%";
+      gradeslength=gradeslength+parseInt(1)-changedlength;
+      addedgrades=parseInt(addedgrades)+parseInt(grade1)-parseInt(changedassignment);
+      grades = addedgrades/gradeslength;
+      document.getElementById('finalgrade').innerText = grades+"%";
     }
     
    
        
   
       
-      document.getElementById('finalgrade').innerText = document.getElementById(clickedid).textContent;
+      
    
   
